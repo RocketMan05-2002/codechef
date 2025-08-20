af36e4e92ed6ef30996d20ca72e31fb961674c1f
Rocketman <abhinayshambharkar1@gmail.com>
2025-08-20 17:34:38 +0530

    airport management

---
+++ 01_array/004_airport_management.cpp
@@ -0,0 +1,37 @@
+//problem link - https://www.codechef.com/problems/AIRM?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int runways(int n, int A[], int D[]){
+    
+    int maxOfA = *max_element(A,A+n);
+    int maxOfD = *max_element(D,D+n);
+    int maxMinuteVal = max(maxOfA,maxOfD);
+    int freq[maxMinuteVal+1] = {0};
+    
+    for(int i=0;i<n;i++){
+        // for(int j= A[i];j<=D[i];j++){
+        //     freq[j]++;
+        // }
+        freq[A[i]]++;
+        freq[D[i]]++;
+    }
+    
+    return *max_element(freq,freq+maxMinuteVal);
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int A[n],D[n];
+	    for(int i=0;i<n;i++)cin>>A[i];
+	    for(int i=0;i<n;i++)cin>>D[i];
+	    cout<<runways(n,A,D)<<endl;
+	}
+	return 0;
+}
