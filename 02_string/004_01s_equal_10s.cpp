ab718ffa112420930377be8ea8b5d55d466b794a
Rocketman <abhinayshambharkar1@gmail.com>
2025-08-23 14:52:13 +0530

    zero ones equal one zeroes

---
+++ 02_string/004_01s_equal_10s.cpp
@@ -0,0 +1,18 @@
+// problem link - https://www.codechef.com/problems/ZOOZ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<"1";
+	    for(int i=0;i<n-2;i++)cout<<"0";
+	    cout<<"1"<<endl;
+	}
+
+}
