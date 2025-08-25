d6dc499feefe5f40c762f994876387fe23f7bc9c
Rocketman <abhinayshambharkar1@gmail.com>
2025-08-25 23:45:47 +0530

    super increasing array

--- 01_array/006_superincreasing.cpp
+++ 01_array/006_superincreasing.cpp
@@ -4,18 +4,25 @@
 #include <math.h>
 using namespace std;
 
-bool isPossible(int n, int k, int x){
-    if(pow(2,k-1)<=x) return true;
-    else return false;
+bool isPossible(int n, int k, int x)
+{
+	if (pow(2, k - 1) <= x)
+		return true;
+	else
+		return false;
 }
 
-int main() {
+int main()
+{
 	int t;
-	cin>>t;
-	while(t--){
-	    int n,k,x;
-	    cin>>n>>k>>x;
-	    if(isPossible(n,k,x)) cout<<"Yes"<<endl;
-	    else cout<<"No"<<endl;
+	cin >> t;
+	while (t--)
+	{
+		int n, k, x;
+		cin >> n >> k >> x;
+		if (isPossible(n, k, x))
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
 	}
 }
